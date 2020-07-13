04e1a8328f856a2bc8515e41a9e759f51d20e879
fadlisabda1 <fadlisabda1@yahoo.com>
2020-07-13 20:08:42 +0700

    menambahkan class vertex

--- main.cpp
+++ main.cpp
@@ -1,5 +1,18 @@
 #include <iostream>
 using namespace std;
+
+class vertex{//akan digunakan sebagai representasi objek vertex atau node didalam sebuah graph.
+    public:
+        char lab;//karakter nama dari vertex yang bersangkutan.
+        bool condition;//berfungsi untuk mengetahui apakah vertex tersebut telah di telusuri ataukah tidak.
+        vertex *parent;//digunakan untuk menelusuri alamat vertex yang belum dilewati.
+
+        vertex(char l){//nantinya akan digunakan ketika tiap vertex dibuat di dalam class graph.
+            lab=l;
+        }
+
+};
+
 int main(){
     
     return 0;
