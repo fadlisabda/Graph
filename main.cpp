e5f0e189f69dddbd082ea1e475e981bbb61cee35
fadlisabda1 <fadlisabda1@yahoo.com>
2020-07-13 23:48:57 +0700

    menambahkan istilah graph

--- main.cpp
+++ main.cpp
@@ -2,7 +2,11 @@
 #include <iostream>
 using namespace std;
 
-
+//vertex merupakan himpunan node atau titik pada sebuah graph
+//edge adalah garis yang menghubungkan tiap vertex pada sebuah graph
+//adjacent dua buah vertex berdekatan(adjacent) dan terhubung  dengan satu garis
+//cycle siklus atau sirkuit yaitu lintasan yang berawal dan berakhir pada simpul yang sama
+//weight bobot nilai yang dimiliki oleh sebuah lintasan(edge)antara dua buah vertex yang terhubung
 
 class vertex{//akan digunakan sebagai representasi objek vertex atau node didalam sebuah graph.
     public:
